d789d6a5176811f9c44fc042ebc7ddd9c7d38461
Kevin Lie <kevinyilie@gmail.com>
2023-06-05 14:55:14 -0400

    Add Side and Square class

---
+++ Side.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Letter_Boxed_Solver
+{
+    public class Side
+    {
+        public Side(char letter0, char letter1, char letter2)
+        {
+            Letters = new char[] { letter0, letter1, letter2 };
+            unusedLetters = Letters.ToHashSet();
+        }
+        public char[] Letters { get; }
+       
+        private HashSet<char> usedLetters = new HashSet<char>();
+        private HashSet<char> unusedLetters;
+
+        public void Play(string word)
+        {
+            foreach (char letter in UnusedLetters())
+            {
+                if (word.Contains(letter))
+                {
+                    usedLetters.Add(letter);
+                    unusedLetters.Remove(letter);
+                }
+            }
+        }
+        public char[] UsedLetters()
+        {
+            return usedLetters.ToArray();
+        }
+
+        public char[] UnusedLetters()
+        {
+            return unusedLetters.ToArray();
+        }
+
+        public bool IsUsed()
+        {
+            return unusedLetters.Count() == 0;
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Letter_Boxed_Solver
+{
+    public class Square
+    {
+        public Square(Side side0, Side side1, Side side2, Side side3)
+        {
+            Sides = new Side[] { side0, side1, side2, side3 };
+
+            for (int i = 0; i < Sides.Length; i++)
+            {
+                foreach (char letter in Sides[i].Letters)
+                {
+                    letterSideDict[letter] = i;
+                }
+            }
+
+            Letters = letterSideDict.Keys.ToHashSet();
+        }
+
+        public Side[] Sides { get; }
+        public HashSet<char> Letters { get; } 
+        private Dictionary<char, int> letterSideDict = new Dictionary<char, int>();
+
+        public void Play(string word)
+        {
+            foreach (Side side in Sides)
+            {
+                side.Play(word);
+            }
+        }
+        
+        public bool IsValidWord(string word)
+        {
+            HashSet<char> wordLetters = new HashSet<char>(word);
+            return wordLetters.IsSubsetOf(Letters);
+        }
+        public int IndexOf(char letter)
+        {
+            if (!letterSideDict.ContainsKey(letter))
+            {
+                return -1;
+            }
+            return letterSideDict[letter];
+        }
+
+        public bool IsUsed()
+        {
+            return Sides.All(side => side.IsUsed());
+        }
+    }
+}
