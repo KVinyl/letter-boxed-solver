eddc3f2a46f09eee4b95cfd6eb8bfed87a0836d7
Kevin Lie <kevinyilie@gmail.com>
2023-06-19 18:31:58 -0400

    Add WordFilter class

---
+++ WordFilter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Letter_Boxed_Solver
+{
+    public class WordFilter
+    {
+        public WordFilter()
+        {
+            ExtractWords();
+        }
+      
+
+        private string filename = "wordfilter.txt";
+        public SortedSet<string> Words { get; private set; } = new();
+
+        public void ExtractWords()
+        {
+            try
+            {
+                using (StreamReader sr = new(filename))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Words.Add(line.ToLower());
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void AddWord(string wordToAdd)
+        {
+            Words.Add(wordToAdd);
+            UpdateFile();
+        }
+
+        public void UpdateFile()
+        {
+            try
+            {
+                using (StreamWriter sw = new(filename))
+                {
+                    foreach (string word in Words)
+                    {
+                        sw.WriteLine(word);
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+}
