cd83ae864d866933fbdfa4b13b91f3b0d82c8ca0
Kevin Lie <kevinyilie@gmail.com>
2023-06-19 18:48:26 -0400

    Change WordDatabase.Value type from HashSet to SortedSet

--- WordDatabase.cs
+++ WordDatabase.cs
@@ -14,7 +14,7 @@ namespace Letter_Boxed_Solver
 {
     public class WordDatabase
     {
-        private Dictionary<char, HashSet<string>> wordDatabase = new Dictionary<char, HashSet<string>>();
+        private Dictionary<char, SortedSet<string>> wordDatabase = new();
 
         /// words.txt is a file generated from the following URL:
         // https://github.com/dwyl/english-words/blob/master/words_alpha.txt
@@ -36,9 +36,7 @@ namespace Letter_Boxed_Solver
         {
             get
             {
-                List<string> wordList = wordDatabase[char.ToUpper(ch)].ToList();
-                wordList.Sort();
-                return wordList.ToArray();
+                return wordDatabase[char.ToUpper(ch)].ToArray();
             }
         }
         public int WordCount { get { return wordDatabase.Sum(x => x.Value.Count); } }
@@ -47,7 +45,7 @@ namespace Letter_Boxed_Solver
         {
             foreach (char letter in letters)
             {
-                wordDatabase[char.ToUpper(letter)] = new HashSet<string>();
+                wordDatabase[char.ToUpper(letter)] = new SortedSet<string>();
             }
         }
 
@@ -97,7 +95,7 @@ namespace Letter_Boxed_Solver
             if (!string.IsNullOrEmpty(word))
             {
                 char firstLetter = word.ToUpper()[0];
-                if (wordDatabase.TryGetValue(firstLetter, out HashSet<string> value))
+                if (wordDatabase.TryGetValue(firstLetter, out SortedSet<string> value))
                 {
                     value.Add(word);
                 }
@@ -109,18 +107,20 @@ namespace Letter_Boxed_Solver
             if (!string.IsNullOrEmpty(word))
             {
                 char firstLetter = word.ToUpper()[0];
-                if (wordDatabase.TryGetValue(firstLetter, out HashSet<string> value))
+                if (wordDatabase.TryGetValue(firstLetter, out SortedSet<string> value))
                 {
                     return value.Remove(word);
                 }
             }
+
             return false;
         }
 
         public string[] AllWords()
         {
             List<string> wordList = new();
-            foreach (HashSet<string> wordSet in wordDatabase.Values)
+
+            foreach (SortedSet<string> wordSet in wordDatabase.Values)
             {
                 wordList.AddRange(wordSet);
             }
